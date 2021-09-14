b3d37eebca3cc3913ccf187e12903c7a66ed9d61
jpkuro <joseluispalmapiguave@gmail.com>
2021-09-13 22:22:51 -0500

    pagina de inicio

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Restauran</title>
+</head>
+<header>
+	<ul>
+		<li>inicio</li>
+		<li>catalogo</li>
+		<li>contacto</li>
+		<li>redes sociales</li>
+	</ul>
+</header>
+<body>
+
+</body>
+</html>
